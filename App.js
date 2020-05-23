ff4784d25e2371cea8afee0bfce62d6dcd65b4da
Semilore Idowu <semijacks27@gmail.com>
2020-05-23 14:16:25 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TestScreen from "./src/screens/TestScreen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName="Test">
+        <Stack.Screen name="Test" component={TestScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/screens/TestScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const TestScreen = () => {
+  return (
+    <View>
+      <Text>Hello from TestScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TestScreen;
