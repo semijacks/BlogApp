87e387367da7e07ef79e1c7e1b0bb26e6886a20c
Semilore Idowu <semijacks27@gmail.com>
2020-07-06 21:43:59 +0100

    started using context

--- App.js
+++ App.js
@@ -2,25 +2,31 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import TestScreen from "./src/screens/TestScreen";
+import IndexScreen from "./src/screens/IndexScreen";
+import {BlogProvider} from "./src/context/BlogContext";
 
 const Stack = createStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer style={styles.container}>
-      <Stack.Navigator initialRouteName="Test">
-        <Stack.Screen name="Test" component={TestScreen} />
+      <Stack.Navigator initialRouteName="Index">
+        <Stack.Screen name="Index" component={IndexScreen}  options={{ title: 'Blogs' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  container: { alignItems: "center",
     justifyContent: "center",
   },
 });
+
+export default () => {
+  return (
+  <BlogProvider>
+    <App />
+    </BlogProvider>
+    ) ;
+};
